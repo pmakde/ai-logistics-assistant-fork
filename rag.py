a03491a383d629eca203d22cbb7d262064a7c7b2
nih8 <b24cs1045@iitj.ac.in>
2026-02-03 00:32:36 +0530

    working on rag but not successful yet

--- rag.py
+++ rag.py
@@ -1,14 +1,38 @@
 import requests
-from langchain.chat_models import init_chat_model
+import os
 import config # This triggers your os.environ logic
 from langchain.messages import HumanMessage, AIMessage, SystemMessage
+from config import text
+from langchain_chroma import Chroma
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+def split_text(text, chunk_size=1000, chunk_overlap=150):
+    chunks = []
+    start = 0
+    while start < len(text):
+        end = start + chunk_size
+        chunks.append(text[start:end])
+        start += (chunk_size - chunk_overlap)
+    return chunks
 
-model = init_chat_model(
-    model='gemini-2.5-flash', 
-    model_provider='google_genai',
-    temperature=0.1
+chunks = split_text(text)
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+
+embeddings = GoogleGenerativeAIEmbeddings(
+    model="models/embedding-001",
 )
+vector_store = Chroma.from_texts(
+    texts=chunks,
+    embedding=embeddings,
+    persist_directory="./chroma_db"
+)
+vector_store.persist()
+retriever = vector_store.as_retriever(search_kwargs={"k": 3})
+user_query = input("What do you wanna know about Zendaya?")
+results = retriever.invoke(user_query)
+for i, doc in enumerate(results):
+    print(f"\n--- Snippet {i+1} ---")
+    print(doc.page_content)
+
+
 
 # Use .content for the most reliable streaming output across providers
-for chunk in model.stream('Hello, what is python?'):
-    print(chunk.content, end='', flush=True)
